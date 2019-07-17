e67d0b09850adb7f48d3a244ba3b77413871f16b
Justin Silverman <jsilve24@gmail.com>
2019-07-16 20:12:18 -0400

    Now Working!

---
+++ R/RcppHungarian.R
@@ -0,0 +1,12 @@
+#' RcppHungarian
+#' 
+#' Header Library and R Functions to Solve Minimum Cost Bipartite Matching Problem 
+#' using Huhn-Munkres algoritym (Hungarian Algorithym). This is a repackaging of code 
+#' written by Cong Ma in the GitHub repo mcximing/hungarian-algorithm-cpp.
+#'  
+#' @docType package
+#' @name RcppHungarian_package
+#' 
+#' @useDynLib RcppHungarian
+#' @importFrom Rcpp sourceCpp
+NULL
--- tests/testthat/test-Hungarian.R
+++ tests/testthat/test-Hungarian.R
@@ -5,7 +5,7 @@ test_that("Algorithm Correctness Simple", {
   soln <- RcppHungarian::HungarianSolver(cost)
   
   expect_equal(soln$cost,0)
-  expect_equal(soln$assignments, matrix(c(0,0,1,1), 2,2, byrow = TRUE))
+  expect_equal(soln$pairs, matrix(c(0,0,1,1), 2,2, byrow = TRUE)+1)
 })
 
 
@@ -16,9 +16,9 @@ test_that("Algorithm Correctness Difficult", {
   soln <- HungarianSolver(cost)
   
   expect_equal(soln$cost,1)
-  expect_equal(soln$assignments, rbind(c(0, 2), 
+  expect_equal(soln$pairs, rbind(c(0, 2), 
                                        c(1, 1), 
-                                       c(2, 0)))
+                                       c(2, 0))+1)
 })
 
 
@@ -26,6 +26,6 @@ test_that("Algorithm Correctness Degenerate", {
   cost <- matrix(1, 2,2)
   soln <- HungarianSolver(cost)
   
-  expect_equal(soln$cost,0)
-  expect_equal(soln$assignments, matrix(c(0,0,1,1), 2,2, byrow = TRUE))
+  expect_equal(soln$cost,2)
+  expect_equal(soln$pairs, matrix(c(0,0,1,1), 2,2, byrow = TRUE)+1)
 })
