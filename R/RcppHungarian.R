709c7103b974a0e0d398d8d84685869153a049c5
Justin Silverman <jsilve24@gmail.com>
2019-07-23 18:48:07 -0400

    Still Fixing for CRAN

--- R/RcppHungarian.R
+++ R/RcppHungarian.R
@@ -2,10 +2,10 @@
 #' 
 #' Header Library and R Functions to Solve Minimum Cost Bipartite Matching 
 #' Problem using Huhn-Munkres algorithm (Hungarian algorithm; 
-#' https://en.wikipedia.org/wiki/Hungarian_algorithm;
+#' <https://en.wikipedia.org/wiki/Hungarian_algorithm>;
 #' Kuhn (1955) doi:10.1002/nav.3800020109).
 #' This is a repackaging of code written by Cong Ma in the GitHub 
-#' repo https://github.com/mcximing/hungarian-algorithm-cpp.
+#' repo <https://github.com/mcximing/hungarian-algorithm-cpp>.
 #'  
 #' @docType package
 #' @name RcppHungarian_package
